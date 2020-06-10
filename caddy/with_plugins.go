7051ee07a0241ca3638256c996aa956d6a0a1ee4
Guoxi <guoxiwang1996@gmail.com>
2020-06-10 23:53:03 +0800

    change wrong github address

--- caddy/with_plugins.go
+++ caddy/with_plugins.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	//"github.com/mholt/caddy/caddy/caddymain"
-	"github.com/GuoxiW/caddy/tree/master/caddy/caddymain"
+	"github.com/GuoxiW/caddy/caddy/caddymain"
 
 	// plug in plugins here
 	//_ "github.com/captncraig/cors/caddy"
-	_ "github.com/GuoxiW/cors/tree/master/caddy"
+	_ "github.com/GuoxiW/cors/caddy"
 )
 
 func main() {
